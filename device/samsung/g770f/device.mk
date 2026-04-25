b2885674953782ce96556b49fa179a8c0cf90bd9
georgijdokin10-netizen <georgijdokin10@gmail.com>
2026-04-25 17:10:23 +0300

    Initial commit: device tree and workflow (no blobs)

---
+++ device/samsung/g770f/device.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/samsung/g770f/system,system) \
+    $(call find-copy-subdir-files,*,vendor/samsung/g770f/vendor,vendor)
+
+PRODUCT_KNOX_DISABLE := true
+
+PRODUCT_NAME := g770f
+PRODUCT_DEVICE := g770f
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G770F
+PRODUCT_MANUFACTURER := samsung
