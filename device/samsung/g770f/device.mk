50858b97ad049ea7d7077989066aa18fe12fbbf9
georgijdokin10-netizen <georgijdokin10@gmail.com>
2026-04-25 20:43:35 +0300

    device.mk

--- device/samsung/g770f/device.mk
+++ device/samsung/g770f/device.mk
@@ -1,9 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/samsung/g770f/system,system) $(call find-copy-subdir-files,*,vendor/samsung/g770f/vendor,vendor)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/samsung/g770f/system,system) \
+    $(call find-copy-subdir-files,*,vendor/samsung/g770f/vendor,vendor)
+
 PRODUCT_KNOX_DISABLE := true
+
 PRODUCT_NAME := g770f
 PRODUCT_DEVICE := g770f
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G770F
 PRODUCT_MANUFACTURER := samsung
+
+# Дополнительные настройки для One UI 7 (порт)
+PRODUCT_SHIPPING_API_LEVEL := 35
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
